94d5a7771fb4e0eb45f0f45dd62eaed07fef368a
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2023-06-14 10:28:35 +0900

    Delete unused files in backend/Router

--- server/backend/Router/return.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    const text = req.query.text;
-    res.send({test: text});
-});
-
-module.exports = router;
